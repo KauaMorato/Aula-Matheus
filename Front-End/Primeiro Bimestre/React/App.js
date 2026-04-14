e71c25790e14e1aa2cb0d87b48b89cb7ab6df894
Kauã Morato <dxrkitslove4@gmail.com>
2026-04-14 15:01:09 -0300

    Nova atividade

---
+++ Front-End/Primeiro Bimestre/React/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Dashboard from './React/cdpages/Dashboard';
+import Relatorios from './React/cdpages/Relatorios';
+import Configuracoes from './React/cdpages/Configuracoes';
+import Suporte from './React/cdpages/Suporte';
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Dashboard</Link></li>
+                        <li><Link to="/relatorios">Relatórios</Link></li>
+                        <li><Link to="/configuracoes">Configurações</Link></li>
+                        <li><Link to="/suporte">Suporte</Link></li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/relatorios" element={<Relatorios />} />
+                    <Route path="/configuracoes" element={<Configuracoes />} />
+                    <Route path="/suporte" element={<Suporte />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
---
+++ Front-End/Primeiro Bimestre/React/cdpages/Suporte.js
@@ -0,0 +1,5 @@
+function Suporte() {
+    return <h1>Suporte</h1>
+}
+
+export default Suporte;
