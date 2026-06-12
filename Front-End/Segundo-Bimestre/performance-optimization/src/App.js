603593361f96ba0a63eb25311100ea6b1bf960e1
Kauã Morato <dxrkitslove4@gmail.com>
2026-06-12 20:20:43 -0300

    Arrumando o código

--- Front-End/Segundo-Bimestre/performance-optimization/src/App.js
+++ Front-End/Segundo-Bimestre/performance-optimization/src/App.js
@@ -21,27 +21,4 @@ function App() {
   );
 }
 
-export default App;import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-const Home = React.lazy(() => import('./components/Home'));
-const About = React.lazy(() => import('./components/About'));
-
-function App() {
-  return (
-    <Router>
-      <nav style={{ padding: '10px' }}>
-        <Link to="/">Início</Link> | <Link to="/about">Sobre</Link>
-      </nav>
-
-      <Suspense fallback={<div>Carregando...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Suspense>
-    </Router>
-  );
-}
-
 export default App;
